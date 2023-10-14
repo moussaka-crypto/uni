39b2f2ed7456d3338694f2f6db2652aa3c7d48ed
Hristomir Dimov <64573585+moussaka-crypto@users.noreply.github.com>
2023-10-14 17:42:10 +0200

    Update position_Buchstabe.cpp

--- year1/gip/Praktikum/P_INF-02.05/position_Buchstabe.cpp
+++ year1/gip/Praktikum/P_INF-02.05/position_Buchstabe.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
-using namespace std;
 
-int main()
-{
+int main() {
+    char letter;
+    std::cout << "Geben Sie einen Kleinbuchstaben ein: ";
+    std::cin >> letter;
 
+    if (letter >= 'a' && letter <= 'z') {
+        int position = letter - 'a' + 1;
+        std::cout << "Die Position des Buchstabens " << letter << " im Alphabet ist: " << position << std::endl;
+    } 
+    else {
+        std::cout << "Ungültige Eingabe. Bitte geben Sie einen Kleinbuchstaben ein." << std::endl;
+    }
+
+    return 0;
 }
