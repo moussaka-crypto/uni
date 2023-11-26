a062558b4fe82b9524232e9949ab4c75c497eb02
Hristomir Dimov <64573585+moussaka-crypto@users.noreply.github.com>
2023-11-26 22:43:48 +0100

    Delete year1/gip/Leftovers/MCD_Probeklausur directory

--- year1/gip/Leftovers/MCD_Probeklausur/expert_math.cpp
+++
@@ -1,6 +0,0 @@
-#include "expert_math.h"
-
-double kreisumfang(double radius)
-{
-	return 2.0 * pi * radius;
-}
--- year1/gip/Leftovers/MCD_Probeklausur/expert_math.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-const double pi = 3.14159265;
-double kreisumfang(double radius = 0.0);
